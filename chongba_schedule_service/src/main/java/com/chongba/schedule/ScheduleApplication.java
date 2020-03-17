5cc7de391f2704448b44f5156883bbaee2bbfbc8
Rainbow Cat <2056423011@qq.com>
2020-03-17 16:34:47 +0800

    充吧工程搭建

---
+++ chongba_schedule_service/src/main/java/com/chongba/schedule/ScheduleApplication.java
@@ -0,0 +1,16 @@
+package com.chongba.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/3/17 16:25
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class ScheduleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( ScheduleApplication.class, args );
+    }
+}
