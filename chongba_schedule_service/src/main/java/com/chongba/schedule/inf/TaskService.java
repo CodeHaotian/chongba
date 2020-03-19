4e0f80bfb42ab3731c461a01c2a6d9e9c8a43c1a
Rainbow Cat <2056423011@qq.com>
2020-03-19 21:00:01 +0800

    任务服务

---
+++ chongba_schedule_service/src/main/java/com/chongba/schedule/inf/TaskService.java
@@ -0,0 +1,30 @@
+package com.chongba.schedule.inf;
+
+import com.chongba.entity.Task;
+import com.chongba.exception.ScheduleSystemException;
+import com.chongba.exception.TaskNotExistException;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/3/19 20:57
+ * @Description: 任务服务
+ */
+public interface TaskService {
+    /**
+     * 添加任务
+     *
+     * @param task 任务对象
+     * @return 任务id
+     * @throws ScheduleSystemException 系统异常
+     */
+    long addTask(Task task) throws ScheduleSystemException;
+
+    /**
+     * 取消任务
+     *
+     * @param taskId 任务id
+     * @return 取消结果
+     * @throws TaskNotExistException 任务异常
+     */
+    boolean cancelTask(long taskId) throws TaskNotExistException;
+}
