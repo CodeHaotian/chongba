407e9f680522014ef60d47886f6b1c299e04db5b
Rainbow Cat <2056423011@qq.com>
2020-04-13 21:40:01 +0800

    配置转移至consul配置中心

--- chongba_schedule_job/src/main/java/com/chongba/job/JobApplication.java
+++ chongba_schedule_job/src/main/java/com/chongba/job/JobApplication.java
@@ -6,10 +6,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * @Author: Haotian
- * @Date: 2020/4/10 17:03
- * @Description: 分布式任务调度中心启动类
- */
+ * 分布式任务调度中心启动类
+ *
+ * @author Haotian
+ * @version 1.0.0
+ * @date 2020/4/13 21:38
+ **/
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients(basePackages = {"com.chongba.feign"})
